6fdfd85383628872620276a4f2e52d9e5e974a98
Sila Tamang <46720345+silatamang@users.noreply.github.com>
2025-11-12 11:53:26 -0600

    Create addTwoIntegers.cs

---
+++ 2235. Add Two Integers/addTwoIntegers.cs
@@ -0,0 +1,11 @@
+/*
+Time complexity: O(1) (constant time)
+
+Space complexity: O(1) (constant memory)
+*/
+
+public class Solution {
+    public int Sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
