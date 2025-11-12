99e2eec3c9a1c7ff5a948c93e18b8f42767a11fc
Sila Tamang <46720345+silatamang@users.noreply.github.com>
2025-11-12 11:59:47 -0600

    Create addTwoInteger.py

---
+++ 2235. Add Two Integers/addTwoInteger.py
@@ -0,0 +1,8 @@
+'''
+Time complexity: O(1) (constant time)
+Space complexity: O(1) (constant memory)
+'''
+class Solution:
+    def sum(self, num1: int, num2: int) -> int: 
+        return num1 + num2
+        
