9fed1a8cc97acae8c6354a0c98b1c10e9d7561ce
Sila Tamang <46720345+silatamang@users.noreply.github.com>
2025-11-12 11:55:45 -0600

    Create addTwoIntegers.java

---
+++ 2235. Add Two Integers/addTwoIntegers.java
@@ -0,0 +1,10 @@
+/*
+Time complexity: O(1) (constant time)
+
+Space complexity: O(1) (constant memory)
+*/
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
