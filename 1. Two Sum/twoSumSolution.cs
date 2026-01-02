47fb1c8b7d6b215746137244446124a0b75afad5
Sila Tamang <46720345+silatamang@users.noreply.github.com>
2026-01-01 18:10:16 -0600

    Update twoSumSolution.cs

--- 1. Two Sum/twoSumSolution.cs
+++ 1. Two Sum/twoSumSolution.cs
@@ -1,1 +1,14 @@
-
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+    var map = new Dictionary<int, int>(nums.Length);
+     for(int i = 0; i < nums.Length; i++)
+     {
+        if(map.TryGetValue(target-nums[i], out int index))
+        {
+            return new[] {index, i};
+        }
+        map[nums[i]] = i;
+     }
+     return Array.Empty<int>();
+    }
+}
