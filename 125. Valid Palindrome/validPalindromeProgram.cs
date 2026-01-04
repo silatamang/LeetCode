843b5ed825c34aaff8e564527bdf1d389f9fa42a
Sila Tamang <46720345+silatamang@users.noreply.github.com>
2026-01-03 20:13:05 -0600

    Create validPalindromeProgram.cs

---
+++ 125. Valid Palindrome/validPalindromeProgram.cs
@@ -0,0 +1,40 @@
+string s = "A man, a plan, a canal: Panama";
+var result = Solution.IsPalindrome(s);
+System.Console.WriteLine(result);
+public static class Solution
+{
+   public static bool IsPalindrome(string s)
+    {
+        int left = 0;
+        int right = s.Length - 1;
+
+        while (left < right)
+        {
+            while (left < right && !char.IsLetterOrDigit(s[left]))
+            {
+                left++;
+            }
+
+            while (left < right && !char.IsLetterOrDigit(s[right]))
+            {
+                right--;
+            }
+
+            char l = char.ToLower(s[left]);
+            char r = char.ToLower(s[right]);
+
+            Console.WriteLine($"{l} === {r}");
+
+            if (l != r)
+            {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+}
+
+}
